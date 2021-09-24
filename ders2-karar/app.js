44303dd45edd18fc673583fc1a6ea75c98c60733
Arslnmrv <arslanmerve1619@gmail.com>
2021-09-24 13:02:39 +0300

    js

---
+++ ders2-karar/app.js
@@ -0,0 +1,20 @@
+//======KARAR YAPILARI======
+
+
+//=========IF-ELSE=========
+
+
+const s1=Number(prompt("birinci sayi"));
+const islem=prompt("islemi giriniz");
+const s2=+prompt("ikinci sayi");
+
+if (islem=="+") {
+    sonuc=s1+s2;
+}else if (islem=="-") {
+    sonuc=s1-s2;
+}else if (islem=="*") {
+    sonuc=s1*s2;
+}else if (islem=="/") {
+    sonuc=s1/s2;
+}
+console.log(sonuc);
