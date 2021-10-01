c7b68d3b71d16f34274c7dfd842a323591d9aea5
Arslnmrv <arslanmerve1619@gmail.com>
2021-10-01 22:29:03 +0300

    js

--- ders5-diziler/ornek3-iterasyon/iterasyon.js
+++ ders5-diziler/ornek3-iterasyon/iterasyon.js
@@ -2,15 +2,154 @@
 
 //****FOR DÖNGÜSÜ******
 
-const koordinatlar = [120, 100, -100, 220, 330, -50];
+// const koordinatlar = [120, 100, -100, 220, 330, -50];
 
-let pozitif=0;
-let negatif=0;
+// let pozitif=0;
+// let negatif=0;
 
-for(let i =0; i<koordinatlar.length; i++){
+// for(let i =0; i<koordinatlar.length; i++){
+
+//  koordinatlar[i] < 0 ? negatif=negatif+koordinatlar[i] : pozitif = pozitif+ koordinatlar[i];
+// }
+// console.log(`negatifler toplamı: ${negatif} pozifler toplamı: ${pozitif}`);
+
+/******FOR IN DONGUSU************** */
+// const koordinatlar = [120, 100, -100, 220, 330, -50];
 
 
+// let pozitif=0;
+// let negatif=0;
 
- koordinatlar[i] < 0 ? negatif=negatif+koordinatlar[i] : pozitif = pozitif+ koordinatlar[i];
+// for(let i in i<koordinatlar){
+//  koordinatlar[i] < 0 ? negatif=negatif+koordinatlar[i] : pozitif = pozitif+ koordinatlar[i];
+// }
+// console.log(`negatifler toplamı: ${negatif} pozifler toplamı: ${pozitif}`);
+
+
+// SORU: Bir hayvanat bahçesinde bulunan hayvan türlerinin
+// bir dizide saklandığı varsayalım.Bu hayvanları türüne
+// göre aramamızı sağlayacak ve o türden kaç adet bulunduğunu
+//  ana programa döndürecek fonksiyonu yaziniz. Eğer o
+// türden bir hayvan yok ise bulunamadığını yazdırsın.
+
+const hayvanlar=["aslan","fil","kartal","kanarya","aslan","aslan","timsah"];
+
+const ara = prompt("sayisini merak ettigin hayvanin adini yaz");
+const bulDondur= function(ara) {
+
+let hayvanSayisi=0;
+for(let i in hayvanlar)
+{
+if(hayvanlar[i]==ara)
+{hayvanSayisi++;}
 }
-console.log(`negatifler toplamı: ${negatif} pozifler toplamı: ${pozitif}`);
+hayvanSayisi==0 ? alert("aradiginiz hayvandan bulunamadi"):
+alert(`aradiginiz ${ara} hayvanindan ${hayvanSayisi} tane bulunmaktadir`);
+}
+bulDondur(ara);
+
+/********FOR OF DONGUSU************* */
+/* for of dongusu, for in dongusunun bir cok veri yapisini 
+kapsayacak sekilde guncellenmis halidir.
+ Bu dongude dizi icersindeki veriye erisirken indisleme 
+ kullanmaya gerek yoktur.*/
+
+let arabalar= ["BMW", "Volvo", "Mini"];
+
+for (let i of arabalar) {
+    console.log(i);
+}
+
+
+/*******FOREACH METHODU*************** */
+//! foreach methodu orijinal diziyi bozmaz
+
+let ogrenciler =["Onur","Hakan","Furkan"];
+
+console.log(ogrenciler.forEach((ogrenci)=>console.log(ogrenci)));//undefined
+
+
+ogrenciler.forEach((deger,index)=>console.log(index + ".kisi " + deger + "dir"));
+
+
+ogrenciler.forEach ((x)=>console.log(x));
+
+
+
+/**********MAP METHODU************* */
+
+//!map methodu orijinal diziyi degistirmez
+
+//ornek: butun elemanlarin 3 katini aliniz
+
+const rakamlar =[3,5,3,2,6,7,9];
+console.log(rakamlar.map((x)=>x*3));
+
+console.log( rakamlar.map((x,index)=>`${index+1} .elemanin 3 kati ${x*3}`));
+
+//ornek: butun elemanlarin 3 katini aliniz
+
+//isimler dizisinin icerisindeki her ismi 
+//buyuk harf olarak yeni bir diziye sakla
+
+const isimler = [
+    "Ahmet",
+    "mehmet",
+    "ismet",
+    "SAFFET",
+    "Can",
+    "Canan",
+    "Cavidan",
+  ];
+
+  //map methodu bir dizi dondurur
+
+  const buyuk = isimler.map((isim)=>isim.toUpperCase());
+  console.log(buyuk);
+
+  document.querySelector(".baslik").textContent = buyuk.join("->");
+
+  /************FILTER METHODU********** */
+
+  //ornek 
+
+ // fiyatlar dizisindeki 250den buyuk fiyatlari yazdir
+
+
+ const fiyatlar=[120, 260, 300, 322.5, 400, 768];
+
+ console.log(fiyatlar.filter((t)=>t>250));   
+
+ //-------------- ÖRNEK -------------------
+// maası 4000 $'dan düsük olanlara %50 zam yapmak istiyoruz
+// ve bunu ayri dizi olarak saklamak istiyoruz.
+const maaslar = [3000, 5000, 4000, 6000, 6500];
+
+console.log(maaslar.filter((a)=>a<4000).map((a)=>a*1.5));
+
+//-------------- ÖRNEK -------------------
+const adlar = [
+  "Samet",
+  "Hakkı",
+  "Duygu",
+  "Emrullah",
+  "Bilal",
+  "Ali",
+  "Ahmet",
+  "Hasan",
+  "Defne",
+  "Serdar",
+];
+
+//ilk harfi a ile baslayanlari yazdir
+
+adlar.filter((x)=>x.startsWith("A")).forEach((x)=>console.log(x));
+
+/****************REDUCE METHODU************* */
+
+//! reduce cikis methodudur, reduce den sonra for each calisamz
+
+const maaslarim=[3000, 5000, 4000, 2000, 1000];
+
+
+console.log(maaslarim.reduce((x,y)=> x+y , 0));
---
+++ ders6-nesneler/ornek1-Nesneler/nesneler.js
@@ -0,0 +1,62 @@
+/************NESNELER***************** */
+
+
+//diziler sirali bellek bolgeleridir. Dolayisiyla dizilere erisim index ile olur.
+// Eger diziler karisiksa (nesne) indexle erisim guclugu ortaya cikar bu yuzden =>........
+
+//!OBJECT (nesne)
+
+//nesnelerde key-value (property-value) yapisi kullanilir
+//nesnelerde aradigimiz veriye erismek icin key adini kullanmamiz gerekir
+
+const insan={
+    ad:"Kenan",
+    soyad:"Kul",
+    yas:30,
+    meslek:"developer",
+    diller:["Java","Js","Sql","Html"]
+}
+
+console.log(insan);
+
+//1) dot notasyonu
+console.log(insan.ad);
+console.log(insan.diller);
+//2) koseli parantez ile
+console.log(insan["ad"]);
+console.log(insan["diller"]);
+
+console.log(`Adim:${insan.ad} ve yasim: ${insan.yas}`);
+ 
+//objeye key.valuelar ekleme
+
+insan.konum="Turkiye";
+insan.email="insan@hotmail.com";
+insan["dogumTarihi"]=1991;
+
+console.log(insan);
+
+//*******NESNE************** */
+
+const kisi={
+    ad:"Merve",
+    soyad:"Arslan",
+    dogumTarihi:1990,
+    meslek:"tester",
+    tool:"selenium",
+    ehliyet:true,
+    yasHesapla:function(){//bugunun yilini al=> Date().getFullYear()
+return new Date().getFullYear()-this.dogumTarihi;
+        
+    },
+    ozet:function(){
+      return `${this.ad} ${this.soyad} ${this.yasHesapla()}yasindadir`      
+    }
+
+}
+console.log(kisi);
+console.log(kisi.yasHesapla());
+console.log(kisi.ozet());
+
+
+
