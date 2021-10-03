4c08c6890f4a0573aaf5ae2151fe26f458f4e716
Arslnmrv <arslanmerve1619@gmail.com>
2021-10-03 22:39:42 +0300

    js

--- tvShowHw/script.js
+++ tvShowHw/script.js
@@ -19,11 +19,11 @@ function showIzle(showListe){
   var liste = document.querySelector(".liste");
   showListe.forEach((a) => {
     liste.innerHTML = liste.innerHTML + `<div class=  "card col-md-3" >
-  <img class="card-img-top" src=${a.show.image? a.show.image.medium:""} alt="">
+  <img class="card-img-top" src=${a.image.medium} alt="">
   <div class="card-body">
-    <h5 >${a.show.name}</h5>
+    <h5 >${a.name}</h5>
     
-    <a href=${a.show.url} target="_blank" class="btn btn-success">Detaylar</a>
+    <a href=${a.url} target="_blank" class="btn btn-success">Detaylar</a>
   </div>
 </div>`;
   });
